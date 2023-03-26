f18e6ef5f85ecf80592e675a75eb47eda13fd3d8
kamchatka-volcano <gorelyy@vivaldi.net>
2023-03-26 11:24:36 +0300

    -setting booleans from "true" and "false" strings; -set the version to 0.5.0;

--- include/figcone_shoal/parser.h
+++ include/figcone_shoal/parser.h
@@ -2,9 +2,25 @@
 #define FIGCONE_SHOAL_PARSER_H
 
 #include <figcone_tree/iparser.h>
+#include <figcone_tree/stringconverter.h>
 #include <figcone_tree/tree.h>
 
+namespace figcone {
+template<>
+struct StringConverter<bool> {
+    static std::optional<bool> fromString(const std::string& data)
+    {
+        if (data == "true" || data == "1")
+            return true;
+        if (data == "false" || data == "0")
+            return false;
+        return std::nullopt;
+    }
+};
+} //namespace figcone
+
 namespace figcone::shoal {
+
 class Parser : public IParser {
 public:
     TreeNode parse(std::istream& stream) override;
